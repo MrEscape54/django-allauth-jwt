06c005636e06894a36da8dbf9d940c8222fd2e21
Diego Guzmán <mrescape@gmail.com>
2022-07-08 11:09:34 -0300

    react structure

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
+      <h1 class="text-blue-500 text-3xl font-bold">
         Hello world!
       </h1>
     </div>
